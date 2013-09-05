8585226370598a8214df15ba0585d2fef483839c
cionides <christina.ionides@gmail.com>
2013-09-05 16:29:42 -0500

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,11 +1,6 @@
-
 import java.util.ArrayList;
 import java.util.List;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
  * @author cionides
